d3825e521973c5231b933831eebc5d392715d5f6
VuyisaZondani <shasazondani@gmail.com>
2023-08-09 21:09:12 +0200

    Setting up AppDBConext class and updating the Model User class.

--- RunGroopWebApp/Models/User.cs
+++ RunGroopWebApp/Models/User.cs
@@ -5,5 +5,7 @@
         public int Pace { get; set; }
         public int? Kilos { get; set; }
         public Address? Address { get; set; }
+        public ICollection<Club> Clubs { get; set; }
+        public ICollection<Race> Races { get; set; }
     }
 }
