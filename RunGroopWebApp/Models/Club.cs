e8fd0b048514cde5bb06dcf98e208af73244d0f5
VuyisaZondani <shasazondani@gmail.com>
2023-08-13 14:24:29 +0200

    Adding Views

--- RunGroopWebApp/Models/Club.cs
+++ RunGroopWebApp/Models/Club.cs
@@ -6,16 +6,16 @@ namespace RunGroopWebApp.Models
     public class Club
     {
         public int Id { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public string Image { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public string? Image { get; set; }
         [ForeignKey("Address")]
         public int AddressId { get; set; }
-        public Address Address { get; set; }
+        public Address? Address { get; set; }
         public ClubCategory ClubCategory { get; set; }
         [ForeignKey("User")]
         public string? UserId { get; set; } 
-        public User User { get; set; }
+        public User? User { get; set; }
 
     }
 }
--- RunGroopWebApp/Models/Race.cs
+++ RunGroopWebApp/Models/Race.cs
@@ -6,15 +6,15 @@ namespace RunGroopWebApp.Models
     public class Race
     {
         public int Id { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public string Image { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public string? Image { get; set; }
         [ForeignKey("Address")]
         public int AddressId { get; set; }
-        public Address Address { get; set; }
+        public Address? Address { get; set; }
         public RaceCategory RaceCategory { get; set; }
         [ForeignKey("User")]
         public string? UserId { get; set; }
-        public User User { get; set; }
+        public User? User { get; set; }
     }
 }
