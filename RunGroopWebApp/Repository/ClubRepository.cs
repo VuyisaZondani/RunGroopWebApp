b253b73519712e6a2b77c0ba8764dc195a130dcf
VuyisaZondani <shasazondani@gmail.com>
2023-08-19 20:14:51 +0200

    Creating Interfaces and repositories.

---
+++ RunGroopWebApp/Repository/ClubRepository.cs
@@ -0,0 +1,49 @@
+﻿using RunGroopWebApp.Data;
+using RunGroopWebApp.Data.Interfaces;
+using RunGroopWebApp.Models;
+
+namespace RunGroopWebApp.Repository
+{
+    public class ClubRepository : IClubRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public ClubRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public bool Add(Club club)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(Club club)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Club>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Club> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Club>> GetClubByCity(string city)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Club club)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
