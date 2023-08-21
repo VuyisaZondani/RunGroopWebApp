8e60489aa659a76d7cd5fe1c5ebc19bce78310ab
VuyisaZondani <shasazondani@gmail.com>
2023-08-21 16:25:19 +0200

    Setting up Api

---
+++ RunGroopWebApp/Data/Helpers/CloudinarySettings.cs
@@ -0,0 +1,10 @@
+﻿namespace RunGroopWebApp.Data.Helpers
+{
+    public class CloudinarySettings
+    {
+        public string CloudName { get; set; }
+        public string ApiKey { get; set; }
+        public string ApiSecret { get; set; }
+
+    }
+}
