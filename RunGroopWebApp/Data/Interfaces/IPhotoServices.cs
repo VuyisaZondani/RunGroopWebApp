b74090225157fa60d507dd45c02f0a38e944cbc2
VuyisaZondani <shasazondani@gmail.com>
2023-08-21 16:26:14 +0200

    Added new service.

---
+++ RunGroopWebApp/Data/Interfaces/IPhotoServices.cs
@@ -0,0 +1,10 @@
+﻿using CloudinaryDotNet.Actions;
+
+namespace RunGroopWebApp.Data.Interfaces
+{
+    public interface IPhotoServices
+    {
+        Task<ImageUploadResult> AddPhotoAsync(IFormFile file);
+        Task<DeletionResult> DeletePhotoAsync(string publicId);
+    }
+}
