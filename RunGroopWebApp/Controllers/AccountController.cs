b31b9762c295110402f13a380473508659b59dd6
VuyisaZondani <shasazondani@gmail.com>
2023-08-30 12:10:34 +0200

    Adding a controller.

---
+++ RunGroopWebApp/Controllers/AccountController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using RunGroopWebApp.Data;
+using RunGroopWebApp.Models;
+using RunGroopWebApp.ViewModels;
+
+namespace RunGroopWebApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+        private readonly ApplicationDbContext _context;
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager, ApplicationDbContext context)
+        {
+            _context = context;
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+        [HttpGet]
+        public IActionResult Login()
+        {
+            var response = new LoginViewModel();
+            return View(response);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel loginViewModel)
+        {
+
+        }
+    }
+}
