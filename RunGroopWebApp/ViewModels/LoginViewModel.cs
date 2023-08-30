a6c2c646890b7458348d4eee02041db52b3315e0
VuyisaZondani <shasazondani@gmail.com>
2023-08-30 12:11:26 +0200

    Creating a view model.

---
+++ RunGroopWebApp/ViewModels/LoginViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RunGroopWebApp.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Display(Name = "Email Address")]
+        public string UserName { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
