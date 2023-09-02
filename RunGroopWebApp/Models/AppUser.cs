62a95bbbdb12a5ca9466d8ebeb34e260ec757ddb
VuyisaZondani <shasazondani@gmail.com>
2023-09-03 01:52:51 +0200

    updating account controller and AppUser model.

--- RunGroopWebApp/Models/AppUser.cs
+++ RunGroopWebApp/Models/AppUser.cs
@@ -10,7 +10,7 @@ namespace RunGroopWebApp.Models
         public int? Kilos { get; set; }
         //Links addesses
         [ForeignKey("Address")]
-        public int AddressId { get; set; }
+        public int? AddressId { get; set; }
         public Address? Address { get; set; }
         public ICollection<Club> Clubs { get; set; }
         public ICollection<Race> Races { get; set; }
