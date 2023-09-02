b599dda757815140c777ae788cb41db6aa6e9b33
VuyisaZondani <shasazondani@gmail.com>
2023-09-03 00:35:31 +0200

    Updating view model

--- RunGroopWebApp/ViewModels/LoginViewModel.cs
+++ RunGroopWebApp/ViewModels/LoginViewModel.cs
@@ -5,7 +5,8 @@ namespace RunGroopWebApp.ViewModels
     public class LoginViewModel
     {
         [Display(Name = "Email Address")]
-        public string UserName { get; set; }
+        [Required(ErrorMessage="Email address is required")]
+        public string EmailAddress { get; set; }
         [Required]
         [DataType(DataType.Password)]
         public string Password { get; set; }
