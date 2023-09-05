c41806988f980cde218566345f795d0c3845a83b
VuyisaZondani <shasazondani@gmail.com>
2023-09-05 20:20:52 +0200

    .

--- RunGroopWebApp/Controllers/DashBoardController.cs
+++ RunGroopWebApp/Controllers/DashBoardController.cs
@@ -1,9 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RunGroopWebApp.Data;
 
 namespace RunGroopWebApp.Controllers
 {
-    public class DashBoardController : Controller
+    public class DashboardController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public DashboardController(ApplicationDbContext context, IUserService)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View();
