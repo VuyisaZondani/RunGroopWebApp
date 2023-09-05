6bc6d9614033d0944d4caa68b95cef7fac48c2c4
VuyisaZondani <shasazondani@gmail.com>
2023-09-05 20:20:18 +0200

    Added interface and repository.

---
+++ RunGroopWebApp/Data/Interfaces/IDashboardRepository.cs
@@ -0,0 +1,10 @@
+﻿using RunGroopWebApp.Models;
+
+namespace RunGroopWebApp.Data.Interfaces
+{
+    public interface IDashboardRepository
+    {
+        Task<List<Race>> GetAllUserRaces();
+        Task<List<Club>> GetAllUserClubs();
+    }
+}
---
+++ RunGroopWebApp/Repository/DashboardRepository.cs
@@ -0,0 +1,18 @@
+﻿using RunGroopWebApp.Data.Interfaces;
+using RunGroopWebApp.Models;
+
+namespace RunGroopWebApp.Repository
+{
+    public class DashboardRepository : IDashboardRepository
+    {
+        public Task<List<Club>> GetAllUserClubs()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Race>> GetAllUserRaces()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
