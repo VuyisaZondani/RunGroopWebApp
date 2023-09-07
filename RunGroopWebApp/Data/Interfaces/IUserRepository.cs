1fcc8bbf05fe998112f3a85f48d9e28d7870a819
VuyisaZondani <shasazondani@gmail.com>
2023-09-07 21:54:35 +0200

    Added a view, view model, interface, repository and controller.

---
+++ RunGroopWebApp/Data/Interfaces/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using RunGroopWebApp.Models;
+
+namespace RunGroopWebApp.Data.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<IEnumerable<User>> GetAllUsers();
+        Task<User> GetUserById(string id);
+        bool Add(User user);
+        bool Update(User user);
+        bool Delete(User user);
+        bool Save();
+    }
+}
