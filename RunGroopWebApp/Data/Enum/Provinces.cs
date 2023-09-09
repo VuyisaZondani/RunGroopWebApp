4acf0926fdcc98bd7fa82d8414f3cd516cc204d7
VuyisaZondani <shasazondani@gmail.com>
2023-09-09 12:01:10 +0200

    Added a class.

---
+++ RunGroopWebApp/Data/Enum/Provinces.cs
@@ -0,0 +1,15 @@
+﻿namespace RunGroopWebApp.Data.Enum
+{
+    public enum Provinces
+    {
+        EasternCape = 1,
+        WesternCape,
+        KwaZuluNatal,
+        Gauteng,
+        Limpopo,
+        NorthenCape,
+        FreeState,
+        Mpumelanga,
+        NorthWest
+    }
+}
